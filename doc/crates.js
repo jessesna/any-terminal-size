34c614aa94ac317524e544a4c9e27d8c6bc76fd4
Jesses Gott <jesses.gott.na+any_terminal_size@gmail.com>
2021-12-13 13:26:02 +0100

    initial gh-pages commit

---
+++ doc/crates.js
@@ -0,0 +1,1 @@
+window.ALL_CRATES = ["any_terminal_size"];
---
+++ doc/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["any_terminal_size"] = {"name":"","dirs":[{"name":"windows","dirs":[{"name":"dll","files":["dll_contents.rs"]}],"files":["dll.rs","dll_injection.rs"]}],"files":["lib.rs","windows.rs"]};
+createSourceSidebar();
